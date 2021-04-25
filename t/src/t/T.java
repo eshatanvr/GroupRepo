a89efa193a41d529aae072584c90c5afff58df4e
Ezzaghafoor <ezzaghafoor17@gmail.com>
2021-04-25 20:44:48 +0500

    Task3

--- t/src/t/T.java
+++ t/src/t/T.java
@@ -3,14 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package t;
+package T3;
 
 import java.util.*;
 /**
  *
  * @author PCC
  */
-public class T {
+public class Task3 {
     /**
      * @param args the command line arguments
      */
